a32114bd59f0d6b8f11a907ab06d9458b411c10e
David Heidelberg <david@ixit.cz>
2020-02-11 13:30:05 +0100

    LICENSE: use SPDX standard, correct FSF address
    Signed-off-by: David Heidelberg <david@ixit.cz>

--- io-webp.c
+++ io-webp.c
@@ -1,5 +1,6 @@
 /* GdkPixbuf library - WebP Image Loader
  *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
  * Copyright (C) 2011 Alberto Ruiz
  * Copyright (C) 2011 David Mazary
  * Copyright (C) 2014 Přemysl Janouch
@@ -7,21 +8,6 @@
  * Authors: Alberto Ruiz <aruiz@gnome.org>
  *          David Mazary <dmaz@vt.edu>
  *          Přemysl Janouch <p.janouch@gmail.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
  */
 
 #include <webp/decode.h>
