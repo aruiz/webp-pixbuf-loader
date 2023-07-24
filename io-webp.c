ce1e65f48523302069289ef539da830038073cb2
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2023-07-24 22:43:13 +0900

    io-webp.c: update_func() shall be called for proper signal delivery.

--- io-webp.c
+++ io-webp.c
@@ -189,6 +189,8 @@ stop_load (gpointer data, GError **error)
         {
           if (context->prepare_func)
             context->prepare_func (pb, NULL, context->user_data);
+          if (context->update_func)
+            context->update_func (pb, 0, 0, context->width, context->height, context->user_data);
 
           g_clear_object (&pb);
 
