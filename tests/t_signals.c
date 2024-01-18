7c4d927b40c4e31086ddab3fb37c66605c0aebac
Colin Clark <colin.clark@cclark.uk>
2024-01-18 14:09:51 +0000

    Requested code amendments

--- tests/t_signals.c
+++ tests/t_signals.c
@@ -1,20 +1,27 @@
 #include <gdk-pixbuf/gdk-pixbuf.h>
 
-gint area_prepared_cb_count = 0;
-gint area_updated_cb_count = 0;
-gint closed_cb_count = 0;
-gint size_prepared_cb_count = 0;
+typedef struct
+{
+  gint area_prepared_cb_count;
+  gint area_updated_cb_count;
+  gint closed_cb_count;
+  gint size_prepared_cb_count;
+} SignalsCounters;
 
 void
 area_prepared_cb (GdkPixbufLoader* self, gpointer user_data)
 {
-  area_prepared_cb_count++;
+  SignalsCounters *counters = (SignalsCounters *) user_data;
+
+  counters->area_prepared_cb_count++;
 }
 
 void
 area_updated_cb (GdkPixbufLoader* self, gint x, gint y, gint width, gint height, gpointer user_data)
 {
-  area_updated_cb_count++;
+  SignalsCounters *counters = (SignalsCounters *) user_data;
+
+  counters->area_updated_cb_count++;
 
   g_assert_cmpint (x, ==, 0);
   g_assert_cmpint (y, ==, 0);
@@ -25,13 +32,17 @@ area_updated_cb (GdkPixbufLoader* self, gint x, gint y, gint width, gint height,
 void
 closed_cb (GdkPixbufLoader* self, gpointer user_data)
 {
-  closed_cb_count++;
+  SignalsCounters *counters = (SignalsCounters *) user_data;
+
+  counters->closed_cb_count++;
 }
 
 void
 size_prepared_cb (GdkPixbufLoader* self, gint width, gint height, gpointer user_data)
 {
-  size_prepared_cb_count++;
+  SignalsCounters *counters = (SignalsCounters *) user_data;
+
+  counters->size_prepared_cb_count++;
 
   g_assert_cmpint (width, ==, 200);
   g_assert_cmpint (height, ==, 200);
@@ -45,13 +56,14 @@ main (gint argc, gchar **argv)
   GError *error = NULL;
   gsize pixbuf_size;
   guchar *pixbuf_data;
+  SignalsCounters *counters = g_new0(SignalsCounters, 1);
 
   loader = gdk_pixbuf_loader_new();
 
-  g_signal_connect (G_OBJECT(loader), "area-prepared", G_CALLBACK(area_prepared_cb), NULL);
-  g_signal_connect (G_OBJECT(loader), "area-updated", G_CALLBACK(area_updated_cb), NULL);
-  g_signal_connect (G_OBJECT(loader), "closed", G_CALLBACK(closed_cb), NULL);
-  g_signal_connect (G_OBJECT(loader), "size-prepared", G_CALLBACK(size_prepared_cb), NULL);
+  g_signal_connect (G_OBJECT(loader), "area-prepared", G_CALLBACK(area_prepared_cb), counters);
+  g_signal_connect (G_OBJECT(loader), "area-updated", G_CALLBACK(area_updated_cb), counters);
+  g_signal_connect (G_OBJECT(loader), "closed", G_CALLBACK(closed_cb), counters);
+  g_signal_connect (G_OBJECT(loader), "size-prepared", G_CALLBACK(size_prepared_cb), counters);
 
   g_file_get_contents (g_environ_getenv (env, "TEST_FILE"), (gchar**)&pixbuf_data, &pixbuf_size, &error);
   if (error)
@@ -63,21 +75,23 @@ main (gint argc, gchar **argv)
     g_error ("%s", error->message);
   g_assert (error == NULL);
 
-  g_free(pixbuf_data);
+  g_clear_pointer(&pixbuf_data, g_free);
 
   gdk_pixbuf_loader_close(loader, &error);
   if (error)
     g_error ("%s", error->message);
   g_assert (error == NULL);
 
-  g_object_unref(G_OBJECT(loader));
+  g_clear_object(&loader);
 
-  g_strfreev (env);
+  g_clear_pointer(&env, g_strfreev);
 
-  g_assert_cmpint (area_prepared_cb_count, ==, 1);
-  g_assert_cmpint (area_updated_cb_count, ==, 1);
-  g_assert_cmpint (closed_cb_count, ==, 1);
-  g_assert_cmpint (size_prepared_cb_count, ==, 1);
+  g_assert_cmpint (counters->area_prepared_cb_count, ==, 1);
+  g_assert_cmpint (counters->area_updated_cb_count, ==, 1);
+  g_assert_cmpint (counters->closed_cb_count, ==, 1);
+  g_assert_cmpint (counters->size_prepared_cb_count, ==, 1);
+
+  g_clear_pointer(&counters, g_free);
 
   return 0;
 }
