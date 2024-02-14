63f06a023af567f1b1b5958faf065643f9a37cb3
Alberto Ruiz <aruiz@redhat.com>
2024-02-14 14:53:24 +0100

    io-webp.c: fix emits prepare_func and update_func

--- io-webp.c
+++ io-webp.c
@@ -143,6 +143,9 @@ stop_load (gpointer data, GError **error)
         {
           if (context->prepare_func)
             context->prepare_func (pb, GDK_PIXBUF_ANIMATION (anim), context->user_data);
+          if (context->update_func)
+            context->update_func (pb, 0, 0, context->width, context->height,
+                                  context->user_data);
           ret = TRUE;
         }
 
@@ -174,6 +177,9 @@ stop_load (gpointer data, GError **error)
           return FALSE;
         }
 
+      if (context->prepare_func)
+        context->prepare_func (pb, NULL, context->user_data);
+
       if (icc_data)
         {
           gdk_pixbuf_set_option (pb, "icc-profile", icc_data);
@@ -187,10 +193,9 @@ stop_load (gpointer data, GError **error)
                                          context->buffer->len, &config);
       if (status == VP8_STATUS_OK)
         {
-          if (context->prepare_func)
-            context->prepare_func (pb, NULL, context->user_data);
-
-          g_clear_object (&pb);
+          if (context->update_func)
+            context->update_func (pb, 0, 0, context->width, context->height,
+                                  context->user_data);
 
           ret = TRUE;
         }
@@ -199,6 +204,8 @@ stop_load (gpointer data, GError **error)
           g_set_error (error, GDK_PIXBUF_ERROR, GDK_PIXBUF_ERROR_FAILED,
                        "WebP decoder failed with VP8 status code: %d", status);
         }
+
+      g_clear_object (&pb);
     }
 
   if (context->buffer)
